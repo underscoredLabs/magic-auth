74c2e30bb0ccc596bfce9370e82413daa2892c6c
leon-do <leondo123@gmail.com>
2021-02-19 13:28:59 -0500

    expires in 5 min

--- signMessage.js
+++ signMessage.js
@@ -1,6 +1,6 @@
 const signMessage = async () => {
   const web3 = new Web3(magic.rpcProvider);
-  const message = Math.round(Date.now() / 1000).toString();
+  const message = Math.round((Date.now() / 1000) + 300).toString();
   const from = (await web3.eth.getAccounts())[0];
   const signature = await web3.eth.personal.sign(message, from);
   console.log({ message, signature, from });
